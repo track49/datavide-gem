03e70629d8315aed34a4e2fc30fe2a7bf243677c
Jack Isherwood <jisherwood12@gmail.com>
2017-11-18 15:38:37 -0800

    latest

--- lib/datavide.rb
+++ lib/datavide.rb
@@ -1,37 +1,36 @@
-require './utils.rb'
+require 'utils.rb'
 
 def getFinance(options)
-  getDatavideApi("finance/#{options.category}/#{options.entity}/#{options.period}", options.apiKey)
+  getDatavideApi("finance/#{options[:category]}/#{options[:entity]}/#{options[:period]}", options[:apiKey])
 end
 
 def getEconomics(options)
-  getDatavideApi("economics/#{options.category}/#{options.country}/#{options.indicatorName}", options.apiKey)
+  getDatavideApi("economics/#{options[:category]}/#{options[:country]}/#{options[:indicatorName]}", options[:apiKey])
 end
 
 def getCompanies(options)
-  getDatavideApi("finance/#{options.category}/#{options.entity}/#{options.period}", options.apiKey)
+  getDatavideApi("finance/#{options[:category]}/#{options[:entity]}/#{options[:period]}", options[:apiKey])
 end
 
 def getSports(options)
   request = ""
 
-  if (options.category == "teams")
-      request = "sports/#{options.league}/teams"
+  if (options[:category] == "teams")
+      request = "sports/#{options[:league]}/teams"
   else
-      if ((options.league == "nfl") || (options.league == "mlb"))
-        if (options.category == "players")
-          request = "sports/#{options.league}/players/#{options.position}"
+      if ((options[:league] == "nfl") || (options[:league] == "mlb"))
+        if (options[:category] == "players")
+          request = "sports/#{options[:league]}/players/#{options[:position]}"
         else
-          request = "sports/#{options.league}/#{options.category}/#{options.position}/#{options.entity}"
+          request = "sports/#{options[:league]}/#{options[:category]}/#{options[:position]}/#{options[:entity]}"
         end
       else
-        if (options.category == "players")
-          request = "sports/#{options.league}/players"
+        if (options[:category] == "players")
+          request = "sports/#{options[:league]}/players"
         else
-          request = "sports/#{options.league}/#{options.category}/#{options.entity}"
+          request = "sports/#{options[:league]}/#{options[:category]}/#{options[:entity]}"
         end
       end
   end
-
-  getDatavideApi(request, options.apiKey)
+  getDatavideApi(request, options[:apiKey])
 end
--- lib/utils.rb
+++ lib/utils.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'json'
 
 def getDatavideApi(path, key)
-  response = Net::HTTP.get(URI("http://api.datavide.com/api/#{path}?apikey=#{key}"))
+  puts "http://www.datavide.com/api/#{path}?apikey=#{key}" 
+  response = Net::HTTP.get(URI("http://www.datavide.com/api/#{path}?apikey=#{key}"))
   JSON.parse(response)
 end
